a4074e3ecb35e8664f3f8fe11a77cd48f9b85df0
Mary E Ramirez <156024789+mramir0605@users.noreply.github.com>
2025-10-21 19:30:55 -0400

    Added files to repository

---
+++ ios101-project5-tumblr/PostCell.swift
@@ -0,0 +1,26 @@
+//
+//  PostCell.swift
+//  ios101-project5-tumblr
+//
+//  Created by mary ramirez on 10/21/25.
+//
+
+import UIKit
+
+class PostCell: UITableViewCell {
+    @IBOutlet weak var postImageView: UIImageView!
+    @IBOutlet weak var summaryLabel: UILabel!
+    
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+        
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
---
+++ ios101-project5-tumblr/ViewController.swift
@@ -0,0 +1,89 @@
+//
+//  ViewController.swift
+//  ios101-project5-tumbler
+//
+
+import UIKit
+import Nuke
+import NukeExtensions
+
+class ViewController: UIViewController, UITableViewDataSource {
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return posts.count
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        // Create the cell
+        let cell = tableView.dequeueReusableCell(withIdentifier: "PostCell", for: indexPath) as! PostCell
+
+        let post = posts[indexPath.row]
+
+        cell.summaryLabel.text = post.summary
+        
+        print("post.summary: \(post.summary)")
+        print("cell.summaryLabel.text: \(cell.summaryLabel.text)")
+        
+        if let photo = post.photos.first, let imageView = cell.postImageView {
+            let url = photo.originalSize.url
+            NukeExtensions.loadImage(with: url, into: imageView)
+        }
+
+        // Return the cell for use in the respective table view row
+        return cell
+    }
+    
+
+    @IBOutlet weak var tableView: UITableView!
+    private var posts: [Post] = []
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        fetchPosts()
+        tableView.dataSource = self
+    }
+
+
+
+    func fetchPosts() {
+        let url = URL(string: "https://api.tumblr.com/v2/blog/humansofnewyork/posts/photo?api_key=1zT8CiXGXFcQDyMFG7RtcfGLwTdDjFUJnZzKJaWTmgyK4lKGYk")!
+        let session = URLSession.shared.dataTask(with: url) { data, response, error in
+            if let error = error {
+                print("❌ Error: \(error.localizedDescription)")
+                return
+            }
+
+            guard let statusCode = (response as? HTTPURLResponse)?.statusCode, (200...299).contains(statusCode) else {
+                print("❌ Response error: \(String(describing: response))")
+                return
+            }
+
+            guard let data = data else {
+                print("❌ Data is NIL")
+                return
+            }
+
+            do {
+                let blog = try JSONDecoder().decode(Blog.self, from: data)
+
+                DispatchQueue.main.async { [weak self] in
+
+                    let posts = blog.response.posts
+
+
+                    print("✅ We got \(posts.count) posts!")
+                    for post in posts {
+                        print("🍏 Summary: \(post.summary)")
+                    }
+                    self?.posts = posts
+                    self?.tableView.reloadData()
+
+                }
+
+            } catch {
+                print("❌ Error decoding JSON: \(error.localizedDescription)")
+            }
+        }
+        session.resume()
+    }
+}
